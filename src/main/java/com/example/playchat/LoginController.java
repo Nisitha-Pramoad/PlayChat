a5d49de71f032beb401ec877979be17ccc93e2e3
Nisitha-Munasinghe <ma.nisithapramoad@gmail.com>
2023-07-03 19:51:26 +0530

    first commit

---
+++ src/main/java/com/example/playchat/LoginController.java
@@ -0,0 +1,17 @@
+package com.example.playchat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField txtNewLoginClient;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/example/playchat/PlayChatApplication.java
@@ -0,0 +1,23 @@
+package com.example.playchat;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PlayChatApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(PlayChatApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("PlayChat");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
