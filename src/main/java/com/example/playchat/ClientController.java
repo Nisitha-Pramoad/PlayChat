fae8aa7da9e222c56a8be2540385f60210646eb1
Nisitha-Munasinghe <ma.nisithapramoad@gmail.com>
2023-07-04 09:19:54 +0530

    implement server class

---
+++ src/main/java/com/example/playchat/ClientController.java
@@ -0,0 +1,116 @@
+package com.example.playchat;
+
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientController implements Initializable {
+
+    @FXML
+    private Button button_send;
+
+    @FXML
+    private ScrollPane sp_main;
+
+    @FXML
+    private TextField tf_message;
+
+    @FXML
+    private VBox vBox_message;
+
+    private Server server;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        /*try {
+            server = new Server(new ServerSocket(1234));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("error creating server...");
+        }
+
+        //vbox size change automatically...meka damme nathnam manually scoll karanna wenawa msg eka balanna..
+        vBox_message.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                //msg ekak apu gaman scroll pane eke size ekath wenas wenawa
+                sp_main.setVvalue((Double) newValue);
+            }
+        });
+
+        //server eka haraha client ewana msg vBox eke display karanawa
+        server.receiveMessageFromClient(vBox_message);
+
+        button_send.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                String  messageToSend = tf_message.getText();
+                if (!messageToSend.isEmpty()) {
+                    HBox hBox = new HBox();
+                    hBox.setAlignment(Pos.CENTER_RIGHT);
+                    hBox.setPadding(new Insets(5, 5,5,10));
+
+                    Text text = new Text(messageToSend);
+                    //message eka loku wadinam eka wena line ekakata ganna ona ekayi rawp karanne..
+                    TextFlow textFlow = new TextFlow(text);
+
+                    textFlow.setStyle("-fx-color: rgb(239,242,255); " +
+                            "-fx-background-color: rgb(15,125,242);" +
+                            " -fx-background-radius: 20px;");
+
+                    textFlow.setPadding(new Insets(5,10,5,10));
+                    text.setFill(Color.color(0.934, 0.945, 0.996));
+
+                    hBox.getChildren().add(textFlow);
+                    vBox_message.getChildren().add(hBox);
+
+                    server.sendMessageToClient(messageToSend);
+                    tf_message.clear();
+                }
+            }
+        });*/
+    }
+
+    //message ekak awama eka gui ekata set karano
+    /*public static void addLabel(String messageFromClient, VBox vBox) {
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER_LEFT);
+        hBox.setPadding(new Insets(5, 5,5,10));
+
+        Text text = new Text(messageFromClient);
+        TextFlow textFlow = new TextFlow(text);
+
+        textFlow.setStyle("-fx-background-color: rgb(15,125,242);" +
+                " -fx-background-radius: 20px;");
+        textFlow.setPadding(new Insets(5,10,5,10));
+        hBox.getChildren().add(textFlow);
+
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                vBox.getChildren().add(hBox);
+
+            }
+        });
+    }*/
+}
---
+++ src/main/java/com/example/playchat/Server.java
@@ -0,0 +1,46 @@
+package com.example.playchat;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void serverStart(){
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New Friend Connected");
+                ClientHandler clientHandler = new ClientHandler(socket);
+
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("error in serverStart");
+        }
+    }
+
+    public void closeServer(){
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("error in closeServer");
+        }
+    }
+
+    public static void main(String args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Server server = new Server(serverSocket);
+        server.serverStart();;
+    }
+}
