ce3ca7bcfdfc75b71077e3d33ccc0105133d5525
Nisitha-Munasinghe <ma.nisithapramoad@gmail.com>
2023-07-14 19:21:54 +0530

    image sending fucntion implementation done

--- src/main/java/com/example/playchat/ClientController.java
+++ src/main/java/com/example/playchat/ClientController.java
@@ -4,20 +4,29 @@ import com.jfoenix.controls.JFXButton;
 import com.vdurmont.emoji.EmojiParser;
 import javafx.application.Platform;
 import javafx.beans.value.ObservableValue;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.*;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.Socket;
 import java.net.URL;
@@ -54,6 +63,7 @@ public class ClientController implements Initializable {
     private BufferedWriter bufferedWriter;
     private String name;
     private EmojiPicker emojiPicker;
+    private FileChooser fileChooser;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -65,7 +75,7 @@ public class ClientController implements Initializable {
             String messageToSend = tf_message.getText().trim();
             if (!messageToSend.isEmpty()) {
                 sendMessage(name + " : " + messageToSend);
-                addLabel(name + " : " + messageToSend, true);
+                addLabel(name + " : " + messageToSend, MessageType.TEXT, true);
                 tf_message.clear();
             }
         });
@@ -76,18 +86,8 @@ public class ClientController implements Initializable {
         vBox.setLayoutX(390);
         vBox.setLayoutY(127);
         vBox.setStyle("-fx-font-size: 30");
-
         pane.getChildren().add(vBox);
-
         emojiPicker.setVisible(false);
-
-        emoji_button.setOnAction(event -> {
-            if (emojiPicker.isVisible()){
-                emojiPicker.setVisible(false);
-            }else {
-                emojiPicker.setVisible(true);
-            }
-        });
 
         emoji_button.setOnAction(event -> {
             emojiPicker.setVisible(!emojiPicker.isVisible());
@@ -101,9 +101,9 @@ public class ClientController implements Initializable {
             emojiPicker.setVisible(false);
         });
 
-
         setUserName();
         startClient();
+        //configureFileChooser();
     }
 
     public ClientController(String name) {
@@ -129,8 +129,16 @@ public class ClientController implements Initializable {
             try {
                 String message;
                 while ((message = bufferedReader.readLine()) != null) {
-                    final String finalMessage = message;
-                    Platform.runLater(() -> addLabel(finalMessage, false));
+                    if (message.matches(".*\\.(png|jpe?g|gif)$")) {
+                        // Image message
+                        String senderName = message.split("[-]")[0];
+                        String imageUrl = message.split("[-]")[1];
+                        Platform.runLater(() -> addImageLabel(senderName, imageUrl));
+                    } else {
+                        // Text message
+                        final String finalMessage = message;
+                        Platform.runLater(() -> addLabel(finalMessage, MessageType.TEXT, false));
+                    }
                 }
             } catch (IOException e) {
                 closeAll();
@@ -150,7 +158,7 @@ public class ClientController implements Initializable {
         }
     }
 
-    public void addLabel(String messageFromClient, boolean isSentMessage) {
+    public void addLabel(String messageFromClient, MessageType type, boolean isSentMessage) {
         HBox hBox = new HBox();
         Text text = new Text(messageFromClient);
         TextFlow textFlow = new TextFlow(text);
@@ -175,6 +183,46 @@ public class ClientController implements Initializable {
         Platform.runLater(() -> vBox_message.getChildren().add(hBox));
     }
 
+    private void addImageLabel(String senderName, String imageUrl) {
+        ImageView imageView;
+        if (imageUrl.startsWith("http")) {
+            // Image URL
+            Image fxImage = new Image(imageUrl);
+            imageView = new ImageView(fxImage);
+        } else {
+            // Local file path
+            File file = new File(imageUrl);
+            try {
+                BufferedImage bufferedImage = ImageIO.read(file);
+                Image fxImage = SwingFXUtils.toFXImage(bufferedImage, null);
+                imageView = new ImageView(fxImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+
+        // Dynamically adjust the image dimensions based on the original image size
+        double maxWidth = 200;
+        double maxHeight = 200;
+        double imageWidth = imageView.getImage().getWidth();
+        double imageHeight = imageView.getImage().getHeight();
+        double scaleFactor = Math.min(maxWidth / imageWidth, maxHeight / imageHeight);
+
+        imageView.setFitWidth(imageWidth * scaleFactor);
+        imageView.setFitHeight(imageHeight * scaleFactor);
+
+        HBox hBox = new HBox();
+        hBox.setPadding(new Insets(5, 5, 5, 10));
+        hBox.getChildren().add(imageView);
+        hBox.setAlignment(Pos.CENTER_LEFT);
+
+        Platform.runLater(() -> vBox_message.getChildren().add(hBox));
+    }
+
+
+
+
     private void setUserName() {
         lblUserName.setText(name);
     }
@@ -192,7 +240,80 @@ public class ClientController implements Initializable {
         }
     }
 
-    
+    /*private void configureFileChooser() {
+        fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Image");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif")
+        );
+    }*/
+
+    @FXML
+    void btnFileButtonOnAction(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Image");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif")
+        );
+
+        File selectedFile = fileChooser.showOpenDialog(getStage());
+        if (selectedFile != null) {
+            sendImage(selectedFile.getAbsolutePath());
+        }
+    }
+
+    private void sendImage(String filePath) {
+        try {
+            Image image = new Image(new FileInputStream(filePath));
+            ImageView imageView = new ImageView(image);
+
+            // Dynamically adjust the image dimensions based on the original image size
+            double maxWidth = 200;
+            double maxHeight = 200;
+            double imageWidth = imageView.getImage().getWidth();
+            double imageHeight = imageView.getImage().getHeight();
+            double scaleFactor = Math.min(maxWidth / imageWidth, maxHeight / imageHeight);
+
+            imageView.setFitWidth(imageWidth * scaleFactor);
+            imageView.setFitHeight(imageHeight * scaleFactor);
+
+            HBox hBox = new HBox();
+            hBox.setPadding(new Insets(5, 5, 5, 10));
+            hBox.getChildren().add(imageView);
+            hBox.setAlignment(Pos.CENTER_RIGHT);
+
+            vBox_message.getChildren().add(hBox);
+
+            PrintWriter printWriter = new PrintWriter(bufferedWriter);
+            printWriter.println(name + "-" + filePath);
+            printWriter.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
 
+    private Stage getStage() {
+        return (Stage) button_send.getScene().getWindow();
+    }
+
+    @FXML
+    void btnSendOnAction(ActionEvent event) {
+        String messageToSend = tf_message.getText().trim();
+        if (!messageToSend.isEmpty()) {
+            sendMessage(name + " : " + messageToSend);
+            addLabel(name + " : " + messageToSend, MessageType.TEXT, true);
+            tf_message.clear();
+        }
+    }
+
+    @FXML
+    void txtMesssageFieldOnAction(ActionEvent event) {
+        btnSendOnAction(event);
+    }
+
+    private enum MessageType {
+        TEXT,
+        IMAGE
+    }
 }
